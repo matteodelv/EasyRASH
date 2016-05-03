09cb5ebc62b75aebc8c0c0df4fb804a2c54c2518
Filippo Vigani <pippoviga1995@gmail.com>
2016-05-03 15:49:16 +0200

    Added basic routing and folder structure

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) { //GET Requests received on root
   res.send('Hello World!');
 });
+
+app.use('/api', require('./api')) //Routes defined in api folder
+
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./public'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
