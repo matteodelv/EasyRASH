ba6601904c368c349fe919b60cd8719e8e1a67e0
Filippo Vigani <pippoviga1995@gmail.com>
2016-07-02 23:56:19 +0200

    Added submission status. Added hover.css. Paper get now removes annotations if user isn't allowed to see them

--- api/papers.js
+++ api/papers.js
@@ -37,16 +37,40 @@ router.get('/:id', function(req, res) {
 		if (fs.existsSync(eventsFilePath)) {
 			fs.readFile(eventsFilePath, (err, data) => {
 				var events = JSON.parse(data);
+				//User can get paper if it's one of its authors, one of its reviewers or one of its chairs, or if the submission has been accepted
 				var eligible = events.some(event =>
 					event.submissions.some(submission => {
-						return submission.url === req.params.id &&
-							(submission.authors.some(author => author === req.jwtPayload.id) || submission.reviewers.some(reviewer => reviewer === req.jwtPayload.id))
+						return submission.url === req.params.id && (
+							submission.status === 'accepted' ||
+							(submission.authors.some(author => author === req.jwtPayload.id) || submission.reviewers.some(reviewer => reviewer === req.jwtPayload.id) || event.chairs.some(chair => chair === req.jwtPayload.id)))
+					})
+				);
+				//User can see annotations if the paper is accepted, he's a chair, or paper isn't pending and is the author or a pc_member
+				var canSeeAnnotations = events.some(event =>
+					event.submissions.some(submission => {
+						return submission.url === req.params.id && (
+							submission.status === 'accepted' ||
+							(event.chairs.some(chair => chair === req.jwtPayload.id)) ||
+							submission.status !== 'pending' && (submission.authors.some(author => author === req.jwtPayload.id) || (event.pc_members.some(pcmember => pcmember === req.jwtPayload.id))))
 					})
 				);
 				var filePath = path.resolve('storage/papers/' + req.params.id + '.html');
 				if (eligible) {
 					if (fs.existsSync(filePath)) {
-						res.sendFile(filePath);
+						console.log(canSeeAnnotations ? 'User CAN see annotations' : 'User can NOT see annotations');
+						fs.readFile(filePath, "utf-8", function(err, data) {
+							var emptyLineRegex = new RegExp(/^\s*\n/gm);
+							if (!canSeeAnnotations) {
+								//Remove all annotations
+								var annotationsRegex = new RegExp(/(?:<script)(?:.*)(?:type="application\/ld\+json")(?:>)((\s|\S)*?)(?:<\/script>)/igm);
+								var xmlCommentsRegex = new RegExp(/(<!--)((\s|\S)*?)(-->)/igm);
+								var clean = data.replace(annotationsRegex, '').replace(xmlCommentsRegex, '').replace(emptyLineRegex, '');
+								res.send(clean);
+							} else {
+								res.send(data.replace(emptyLineRegex,''));
+							}
+						});
+
 					} else res.status(404).send('404 sorry not found');
 				} else res.status(403).send('403 Forbidden');
 			});
